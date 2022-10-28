a2df86b857e1c27e8963efdd1883697f012d8798
Siyana Zdravkova <52591976+BlueButterflies@users.noreply.github.com>
2022-10-28 12:20:08 +0200

    Menu and first button game
    add menu , scene  missing vowels and scrips

---
+++ Assets/MissingVowelsController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissingVowelsController : MonoBehaviour
+{
+    public Text text;
+    public Text btnFirst;
+    public Text btnSecond;
+    public Text btnThird;
+    public Text btnFourth;
+
+
+}
